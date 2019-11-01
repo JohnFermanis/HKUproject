094e29d7e75d62562c1e348ee135ae084528644a
John Fermanis <i.fermanis@students.uu.nl>
2019-11-01 11:33:34 +0100

    Adding PrototypeTestAR and PrototypeTest2

---
+++ PrototypeTest2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeSceneA()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+
+    void ChangeSceneB()
+    {
+
+    }
+
+    void ChangeSceneC()
+    {
+
+    }
+
+    void ChangeSceneD()
+    {
+
+    }
+
+}
