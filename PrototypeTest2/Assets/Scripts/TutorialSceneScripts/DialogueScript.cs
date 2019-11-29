3ee5dac010dab4c5a4613ed4350831961ad45232
JuliaBerkouwer <36532677+JuliaBerkouwer@users.noreply.github.com>
2019-11-29 16:20:24 +0100

    Text added to the UI of the first scene
    All the dialog text has been added to the canvas of the games

--- PrototypeTest2/Assets/Scripts/TutorialSceneScripts/DialogueScript.cs
+++ PrototypeTest2/Assets/Scripts/TutorialSceneScripts/DialogueScript.cs
@@ -41,7 +41,7 @@ public class DialogueScript : MonoBehaviour
 
     private bool _TextFinished;
 
-    private static int _NumberOfDialogues = 3;
+    private static int _NumberOfDialogues = 10;
 
     private string[] _Dialogue = new string[_NumberOfDialogues];
 
@@ -51,16 +51,16 @@ public class DialogueScript : MonoBehaviour
 
     //The dialogue
 
-    private string _D0 = "This is the dialogue1";
-    private string _D1 = "This is the dialogue2";
-    private string _D2 = "This is the dialogue3";
-    //private string _D3 = "This is the dialogue4";
-    //private string _D4 = "This is the dialogue5";
-    //private string _D5 = "This is the dialogue6";
-    //private string _D6 = "This is the dialogue7";
-    //private string _D7 = "This is the dialogue8";
-    //private string _D8 = "This is the dialogue9";
-    //private string _D9 = "This is the dialogue10";
+    private string _D0 = "Rechter, op xxxxx ben ik ontslagen door Quick Sales. Dit is echter niet gegaan om een acceptabele reden. Quick Sales geeft aan dat ik personen aanbiedingen heb gedaan die niet besluitvaardig zijn.";
+    private string _D1 = "Ik heb echter de personen die de AI mij aanbeval te contacteren gecontacteerd. (Ik heb echter de personen gecontacteerd die de AI mij heeft aanbevolen)Deze AI is blijkbaar niet goed ingesteld.";
+    private string _D2 = "Ik ben er vanuit gegaan dat de door Quick Sales aangeboden AI te vertrouwen is.";
+    private string _D3 = "Bij nader onderzoeken blijkt echter dat de AI een algoritme gebruikt wat personen aandraagt gebaseerd op hun gedrag en of de kans van het accepteren van een aanbieding groot is.";
+    private string _D4 = "De fout ligt dus niet bij mij, maar bij quick sales, die mij een AI heeft gefaciliteerd die de problemen creëert.";
+    private string _D5 = "Rechter, M. Jansen heeft onjuist gehandeld. Ze heeft contracten aangeboden aan personen die dronken en wilsonbekwaam zijn op moment van besluit.";
+    private string _D6 = "Dit is zo veelvuldig gebeurt dat het Quick Sales in de rechtelijke problemen is gekomen. Dit heeft Quick Sales veel tijd en geld gekost.";
+    private string _D7 = "Wij zijn een intern onderzoek gestart naar hoe dit heeft kunnen gebeuren. Hieruit is naar voren gekomen dat mevrouw Jansen vaak aan de personen heeft verkocht waarbij dit niet had mogen gebeuren.";
+    private string _D8 = "Op grond daarvan hebben wij haar arbeidscontract ontbonden.";
+    private string _D9 = "Het gebruikte algoritme draagt alleen mensen aan als potentiële klanten, maar het is aan de verkoper zelf om de persoon te beoordelen. Dit is bij mevrouw Jansen duidelijk nooit goed gebeurt.";
 
     //Corresponding Names for each dialogue
     
@@ -75,27 +75,35 @@ public class DialogueScript : MonoBehaviour
         _Dialogue[0] = _D0;
         _Dialogue[1] = _D1;
         _Dialogue[2] = _D2;
-        //_Dialogue[3] = _D3;
-        //_Dialogue[4] = _D4;
-        //_Dialogue[5] = _D5;
-        //_Dialogue[6] = _D6;
-        //_Dialogue[7] = _D7;
-        //_Dialogue[8] = _D8;
-        //_Dialogue[9] = _D9;
+        _Dialogue[3] = _D3;
+        _Dialogue[4] = _D4;
+        _Dialogue[5] = _D5;
+        _Dialogue[6] = _D6;
+        _Dialogue[7] = _D7;
+        _Dialogue[8] = _D8;
+        _Dialogue[9] = _D9;
 
         _Names[0] = _Name1;
-        _Names[1] = _Name2;
+        _Names[1] = _Name1;
         _Names[2] = _Name1;
-        //_Names[3] = _Name1;
-        //_Names[4] = _Name2;
-        //_Names[5] = _Name1;
+        _Names[3] = _Name1;
+        _Names[4] = _Name1;
+        _Names[5] = _Name2;
+        _Names[6] = _Name2;
+        _Names[7] = _Name2;
+        _Names[8] = _Name2;
+        _Names[9] = _Name2;
 
         _Faces[0] = _Face1;
-        _Faces[1] = _Face2;
+        _Faces[1] = _Face1;
         _Faces[2] = _Face1;
-        //_Faces[3] = _Face1;
-        //_Faces[4] = _Face2;
-        //_Faces[5] = _Face1;
+        _Faces[3] = _Face1;
+        _Faces[4] = _Face1;
+        _Faces[5] = _Face2;
+        _Faces[6] = _Face2;
+        _Faces[7] = _Face2;
+        _Faces[8] = _Face2;
+        _Faces[9] = _Face2;
 
     }
 
