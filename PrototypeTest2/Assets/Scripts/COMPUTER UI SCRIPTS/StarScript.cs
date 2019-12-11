38c2006c405b13bfc96f684986436c56f4af6227
John Fermanis <i.fermanis@students.uu.nl>
2019-12-11 14:30:16 +0100

    Evidence can now be starred

---
+++ PrototypeTest2/Assets/Scripts/COMPUTER UI SCRIPTS/StarScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StarScript : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField]
+    private Image _Image;
+
+    [SerializeField]
+    private Sprite _StarOffSprite;
+
+    [SerializeField]
+    private Sprite _StarOnSprite;
+
+    private bool _StarIsOn=false;
+
+    private void Start()
+    {
+        _Image.sprite = _StarOffSprite;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!_StarIsOn)
+        {
+            _StarIsOn = true;
+            _Image.sprite = _StarOnSprite;
+        }
+        else
+        {
+            _StarIsOn = false;
+            _Image.sprite = _StarOffSprite;
+        }
+    }
+    
+
+}
